58388505e3ca1ae6862fa4f296b8aa620e6820b7
Hugo Ruiz <hruizvil@outlook.com>
2021-03-27 22:56:02 -0700

    Updated table to show for researcher

--- healthcareApp/urls.py
+++ healthcareApp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import homePage
+from .views import homePage, Page2, Page3, equipment
+
+app_name = 'healthcareApp'
 
 urlpatterns = [
-    path('', homePage, name='home')
+    path('', homePage, name='home'),
+    path('page2', Page2, name='page2'),
+    path('page3', Page3, name='page3'),
+    path('equipment/', equipment, name='equipment')
 ]
--- healthcareApp/views.py
+++ healthcareApp/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-from healthcareApp.models import Facility
+from healthcareApp.models import *
 
 def homePage(request):
-    return render(request, 'index.html')
+    return render(request, 'Page1.html')
 
 def facilities_detail_view(request):
     facilities = Facility.objects.all()
-    print(facilities)
     args = {'facilities': facilities}
     return render(request, '../templates/index.html', args)
+
+def Mass_spectrometer_details(request):
+    obj = Mass_spectrometer.objects.all()
+    return render(request, '../templates/index.html', obj)
+
+def equipment(request):
+    # query = request.GET.get('Mass_spectrometer')
+    # if query:
+    #     results = Mass_spectrometer.objects.all()
+    
+    
+
+    # context = RequestContext(request)
+    equipment = Mass_spectrometer.objects.all()
+    facilities = Facility.objects.all()
+    args = {'equipment': equipment, 'facilities': facilities}
+    return render(request, 'equipment.html', args)
+
+def Page2(request):
+    return render(request, 'Page2.html')
+
+def Page3(request):
+    return render(request, 'Page3.html')
